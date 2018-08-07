d6a97f00a54035b25aed41bd16713fb3fe5f63e6
Chris Franklin <cf73021@gmail.com>
2018-08-07 20:45:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,4 +35,8 @@ class Song
   def self.find_by_name(name)
     self.all.find{|person| person.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    create_by_name(name)
+    find_by_name(name)
 end
